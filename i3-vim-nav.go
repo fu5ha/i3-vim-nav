8515f173b2dae003e6c571762c84f91b389a7bf6
Gray Olson <gray@grayolson.com>
2017-07-30 15:21:05 -0700

    allow window switching from insert mode in vim

--- i3-vim-nav.go
+++ i3-vim-nav.go
@@ -26,7 +26,7 @@ func main() {
 	r, _ := regexp.Compile(`\bn?v(im)?$`)
 
 	if r.MatchString(name) {
-		keycmd := exec.Command("xdotool", "key", "--clearmodifiers", "control+"+dir)
+		keycmd := exec.Command("xdotool", "key", "--clearmodifiers", "Escape+control+"+dir)
 		out, _ := keycmd.Output()
 		if len(out) > 0 {
 			fmt.Println(out)
